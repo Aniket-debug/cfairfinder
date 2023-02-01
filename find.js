2ff101128d5af40757e5713e8de07479f2c81943
Aniket Agarwal <83874650+Aniket-debug@users.noreply.github.com>
2023-02-01 22:29:36 +0530

    cfair

---
+++ find.js
@@ -0,0 +1,60 @@
+async function find_AIR() {
+
+    document.getElementById("out").innerHTML = "Loading..";
+
+    let username = document.getElementById("username").value;
+    username = username.trim();
+    let contestId = document.getElementById("contest_id").value;
+    contestId = contestId.trim();
+
+    if (contestId.slice(0, 31) != "https://codeforces.com/contest/") {
+        document.getElementById("out").innerHTML = "Entered Link is not correct";
+        return;
+    }
+    contestId = contestId.slice(31);
+
+    let l = 0;
+
+    while ('0' <= contestId[l] && contestId[l] <= '9') l++;
+
+    contestId = contestId.slice(0, l);
+
+    let response = await fetch("https://codeforces.com/api/contest.standings?contestId=" + contestId + "&from=1&count=700&showUnofficial=false");
+    response = await response.json();
+
+    if (response.status != "OK") {
+        if (response.status == "FAILED")
+            document.getElementById("out").innerHTML = response.comment;
+        else
+            document.getElementById("out").innerHTML = "Entered Link is not correct";
+        return;
+    }
+
+    let userinfo = "https://codeforces.com/api/user.info?handles=";
+
+    for (let user of response.result.rows)
+        userinfo += user.party.members[0].handle + ';';
+
+    response = await fetch(userinfo);
+    response = await response.json();
+
+    let rank = 0;
+    for (let user of response.result) {
+        if (user.country === "India") {
+            rank++;
+            if (user.handle == username) {
+                document.getElementById("out").innerHTML = user.firstName + " Your AIR is: " + rank;
+                return;
+            }
+        }
+        if (user.handle == username) {
+            if (user.country == undefined)
+                document.getElementById("out").innerHTML = "Username you entered didn't mention his/her country";
+            else
+                document.getElementById("out").innerHTML = "Username you entered is not from India";
+            return;
+        }
+    }
+
+    document.getElementById("out").innerHTML = "Not Found";
+}
